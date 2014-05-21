1dabd2c7d919fef33c6ccdd481f6f87d3f388684
Pavel Katovich <pkotovich@gmail.com>
2014-05-21 23:58:08 +0300

    admin: created db.default.php

--- protected/config/admin/components/db.default.php
+++ protected/config/admin/components/db.default.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ *  Template for database configuration file.
+ *  Create db.php on yours server , copy the php code below and replace
+ *  dbname,host,username, password if needed
+ */
+
 return array(
         'connectionString' => 'mysql:host=localhost;dbname=admin_xabina_main',
         'emulatePrepare' => true,
