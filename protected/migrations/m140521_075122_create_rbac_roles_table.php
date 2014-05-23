908aeec453605cdd3b6eef20c6885678fbc09f5b
Pavel Katovich <pkotovich@gmail.com>
2014-05-23 11:18:04 +0300

    added migration create_rbac_roles_table

--- protected/migrations/m140521_075122_create_rbac_roles_table.php
+++ protected/migrations/m140521_075122_create_rbac_roles_table.php
@@ -4,12 +4,19 @@ class m140521_075122_create_rbac_roles_table extends CDbMigration
 {
 	public function up()
 	{
+		$this->execute("CREATE TABLE IF NOT EXISTS `rbac_roles` (
+						  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+						  `name` varchar(255) NOT NULL,
+						  `is_system` tinyint(2) unsigned NOT NULL,
+						  `create_uid` int(11) unsigned NOT NULL,
+						  `parent_id` int(11) unsigned DEFAULT NULL,
+						  PRIMARY KEY (`id`)
+						) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Roles'");
 	}
 
 	public function down()
 	{
-		echo "m140521_075122_create_rbac_roles_table does not support migration down.\n";
-		return false;
+		$this->dropTable('rbac_roles');
 	}
 
 	/*
