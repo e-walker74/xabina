a5866dd73a00e128b86a4cb07d09f20a000deca8
Pavel Katovich <pkotovich@gmail.com>
2014-06-08 10:47:50 +0300

    add user page

--- protected/views/rbac/add_user.php
+++ protected/views/rbac/add_user.php
@@ -1,8 +1,59 @@
-<?php
+<div class="col-lg-9 col-md-9 col-sm-9" >
+    <div class="h1-header">Аdding a new user</div>
+    <div class="role-form xabina-form-container">
+        <div class="account-selection" >
+            <span class="select-lbl pull-left">Счет</span>
+            <div class="select-custom account-select pull-right" style="width: 92%!important">
+                <span class="select-custom-label">
+                    Виктор Купец
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    0185 2156 4657
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    10 000 EUR
+                </span>
+                <select name="" class=" select-invisible">
+                    <option value="">
+                        Виктор Купец
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        0185 2156 4657
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        10 000 EUR
+                    </option>
+                    <option value="">
+                        Виктор Купец
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        0185 2156 4657
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        10 000 EUR
+                    </option>
+                    <option value="">
+                        Виктор Купец
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        0185 2156 4657
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        10 000 EUR
+                    </option>
+                    <option value="">
+                        Виктор Купец
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        0185 2156 4657
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        10 000 EUR
+                    </option>
+                </select>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+            </div>
+        </div>
+    </div>
+</div>
 
