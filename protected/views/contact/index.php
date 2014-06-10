58a2c9dcd03bc6c5a0f6e6b788e413017d2a5ec8
Nikita <nikitagorokhovtsev@gmail.ru>
2014-06-10 21:52:39 +0400

    remove icon for text field

--- protected/views/contact/index.php
+++ protected/views/contact/index.php
@@ -1,7 +1,10 @@
 <div class="col-lg-9 col-md-9 col-sm-9" >
 	<div class="h1-header"><?= Yii::t('Front', 'My contact') ?></div>
 	<div class="contact-search-cont">
-		<input class="contact-input" type="text" />
+        <span class="clear-input-cont">
+		    <input class="contact-input" type="text" />
+            <span class="clear-input-but"></span>
+        </span>
 		<a class="contact-search-but" href="#"></a>
 		<a class="add-contact-but rounded-buttons" href="#"><?= Yii::t('Front', 'Add Contact') ?></a>
 	</div>
