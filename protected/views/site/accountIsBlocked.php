3999fd96403819f68e12d31455a7a0b457db8b21
Ivan Medvedev <chuchello@gmail.com>
2014-07-17 13:57:01 +0400

    Change spelling

--- protected/views/site/accountIsBlocked.php
+++ protected/views/site/accountIsBlocked.php
@@ -6,7 +6,7 @@
         <div class="success-ico"></div>
         <div class="congratulations-msg">
             <?= Yii::t('Front', 'You have entered the wrong SMS-code 3 times. Your profile has been temporarily blocked for 1 hour. Please check Your E-Mail in order to restore access to Your account.<br/>'); ?>
-            <a class="send-again" onclick="resendLoginEmail('<?= Yii::t('Front', 'SMS was sent') ?>', '<?= Yii::app()->createUrl('/site/resendBlockEmail') ?>')" href="javaScript:void(0)"><?= Yii::t('Front', 'Send E-mail again.') ?></a>
+            <a class="send-again" onclick="resendLoginEmail('<?= Yii::t('Front', 'E-mail was sent') ?>', '<?= Yii::app()->createUrl('/site/resendBlockEmail') ?>')" href="javaScript:void(0)"><?= Yii::t('Front', 'Send E-mail again.') ?></a>
 
         </div>
     </div>
