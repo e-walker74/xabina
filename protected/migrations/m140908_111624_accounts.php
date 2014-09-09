ebcfbdb39f22a0138fa74dd2feef56884dd03935
Evgeniy <evgeniy.kazak@gmail.com>
2014-09-09 20:06:13 +0300

    account dialog

--- protected/migrations/m140908_111624_accounts.php
+++ protected/migrations/m140908_111624_accounts.php
@@ -6,45 +6,26 @@ class m140908_111624_accounts extends CDbMigration
 	{
         $this->execute('
             ALTER TABLE `accounts`
-            MODIFY COLUMN `number`  bigint(12) UNSIGNED ZEROFILL NOT NULL AFTER `id`;
+            MODIFY COLUMN `number`  bigint(12) UNSIGNED ZEROFILL NOT NULL AFTER `id`,
+            MODIFY COLUMN `status`  tinyint(4) UNSIGNED NOT NULL AFTER `user_id`,
+            ADD COLUMN `prefix`  enum(\'mca\',\'ba\') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT \'mca\' COMMENT \'ba - basic account, mca - multicurrency account\' AFTER `id`,
+            DROP INDEX `number`,
+            ADD UNIQUE INDEX `number` USING BTREE (`number`, `currency_id`),
+            ADD COLUMN `sub_type`  enum(\'personal\',\'anonymous\') NOT NULL DEFAULT \'personal\' AFTER `type_id`,
+            MODIFY COLUMN `status`  tinyint(4) UNSIGNED NOT NULL DEFAULT 0 AFTER `user_id`,
+            ADD COLUMN `basic`  tinyint(1) UNSIGNED NOT NULL DEFAULT 1 AFTER `status`,
+            ADD COLUMN `status_comment`  varchar(255) NULL AFTER `status`,
+            ADD COLUMN `credit_facility`  double(11,0) UNSIGNED NOT NULL DEFAULT 0 AFTER `name`;
 
-            ALTER TABLE `accounts`
-            MODIFY COLUMN `status`  tinyint(4) UNSIGNED NOT NULL AFTER `user_id`;
+        ');
 
-            ALTER TABLE `accounts`
-MODIFY COLUMN `prefix`  enum(\'mca\',\'ba\') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT \'mca\' COMMENT \'ba - basic account, mca - multicurrency account\' AFTER `id`;
-
-            ALTER TABLE `accounts`
-DROP INDEX `number` ,
-ADD UNIQUE INDEX `number` USING BTREE (`number`, `currency_id`) ;
-
-
-            ALTER TABLE `accounts`
-ADD COLUMN `sub_type`  enum(\'personal\',\'anonymous\') NOT NULL DEFAULT \'personal\' AFTER `type_id`;
-
-            ALTER TABLE `accounts`
-MODIFY COLUMN `status`  tinyint(4) UNSIGNED NOT NULL DEFAULT 0 AFTER `user_id`;
-
-            ALTER TABLE `accounts`
-ADD COLUMN `basic`  tinyint(1) UNSIGNED NOT NULL DEFAULT 1 AFTER `status`;
-
-            ALTER TABLE `accounts`
-ADD COLUMN `status_comment`  varchar(255) NULL AFTER `status`;
-
-            ALTER TABLE `accounts`
-ADD COLUMN `credit_facility`  double(11,0) UNSIGNED NOT NULL DEFAULT 0 AFTER `name`;
-
-            CREATE TABLE `accounts_names` (aa
+        $this->execute('CREATE TABLE `accounts_names` (
             `id`  int(11) NOT NULL AUTO_INCREMENT ,
             `user_id`  int(11) UNSIGNED NULL ,
             `name`  varchar(255) NOT NULL ,
             `lang`  varchar(2) NOT NULL ,
             PRIMARY KEY (`id`)
-            );
-
-        ');
-
-        $this->execute('ALTER TABLE `accounts_names` ADD CONSTRAINT `fk_accounts_names_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;');
+            );');
 	}
 
 	public function down()
